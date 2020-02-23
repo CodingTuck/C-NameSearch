e61883aba2fef91e77db50aca9a50960ced7a66a
mitch <mitch@DESKTOP-5RPTGKT>
2020-02-23 09:20:30 -0500

    Add project files.

---
+++ Name Search/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace Name_Search
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.boyTextBox = new System.Windows.Forms.TextBox();
+            this.girlTextBox = new System.Windows.Forms.TextBox();
+            this.boyLabel = new System.Windows.Forms.Label();
+            this.girlLabel = new System.Windows.Forms.Label();
+            this.checkNameButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // boyTextBox
+            // 
+            this.boyTextBox.Location = new System.Drawing.Point(37, 67);
+            this.boyTextBox.Name = "boyTextBox";
+            this.boyTextBox.Size = new System.Drawing.Size(151, 20);
+            this.boyTextBox.TabIndex = 0;
+            // 
+            // girlTextBox
+            // 
+            this.girlTextBox.Location = new System.Drawing.Point(194, 67);
+            this.girlTextBox.Name = "girlTextBox";
+            this.girlTextBox.Size = new System.Drawing.Size(151, 20);
+            this.girlTextBox.TabIndex = 1;
+            // 
+            // boyLabel
+            // 
+            this.boyLabel.AutoSize = true;
+            this.boyLabel.Location = new System.Drawing.Point(91, 51);
+            this.boyLabel.Name = "boyLabel";
+            this.boyLabel.Size = new System.Drawing.Size(56, 13);
+            this.boyLabel.TabIndex = 2;
+            this.boyLabel.Text = "Boy Name";
+            // 
+            // girlLabel
+            // 
+            this.girlLabel.AutoSize = true;
+            this.girlLabel.Location = new System.Drawing.Point(244, 51);
+            this.girlLabel.Name = "girlLabel";
+            this.girlLabel.Size = new System.Drawing.Size(53, 13);
+            this.girlLabel.TabIndex = 3;
+            this.girlLabel.Text = "Girl Name";
+            // 
+            // checkNameButton
+            // 
+            this.checkNameButton.Location = new System.Drawing.Point(94, 112);
+            this.checkNameButton.Name = "checkNameButton";
+            this.checkNameButton.Size = new System.Drawing.Size(91, 50);
+            this.checkNameButton.TabIndex = 4;
+            this.checkNameButton.Text = "Check Name For Top 200";
+            this.checkNameButton.UseVisualStyleBackColor = true;
+            this.checkNameButton.Click += new System.EventHandler(this.checkNameButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(194, 112);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(91, 50);
+            this.exitButton.TabIndex = 5;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(381, 174);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.checkNameButton);
+            this.Controls.Add(this.girlLabel);
+            this.Controls.Add(this.boyLabel);
+            this.Controls.Add(this.girlTextBox);
+            this.Controls.Add(this.boyTextBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox boyTextBox;
+        private System.Windows.Forms.TextBox girlTextBox;
+        private System.Windows.Forms.Label boyLabel;
+        private System.Windows.Forms.Label girlLabel;
+        private System.Windows.Forms.Button checkNameButton;
+        private System.Windows.Forms.Button exitButton;
+    }
+}
+
---
+++ Name Search/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Name_Search
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void checkNameButton_Click(object sender, EventArgs e)
+        {
+            const int NAMESIZE = 200; //total number of names in each file
+            string[] boyNames = new string[NAMESIZE]; //array of names
+            string[] girlNames = new string[NAMESIZE]; //array of names
+            int index = 0;
+            string boyName;
+            string girlName;
+            Boolean girlInput = false;
+            Boolean boyInput = false;
+            StreamReader boySearchFile; //boy file input
+            StreamReader girlSearchFile; //girl file input
+
+            //get StreamReader object
+            boySearchFile = File.OpenText("BoyNames.txt"); //opens file
+            girlSearchFile = File.OpenText("GirlNames.txt"); //opens file
+
+            girlName = girlTextBox.Text;
+            boyName = boyTextBox.Text; //initialize boyName with user input
+            if (string.IsNullOrEmpty(boyTextBox.Text) == boyInput) //tests if there is any name typed in boyTextBox
+            {
+
+                while (index < boyNames.Length && !boySearchFile.EndOfStream)
+                {
+                    boyNames[index] = boySearchFile.ReadLine();//initialize 0 of the file to string
+                    if (boyName == boyNames[index])//if number length of array equals user input then message box shows
+                    {
+                        //Message box showing user input equals a name in the file
+                        MessageBox.Show(boyTextBox.Text + " is in the top 200 names for boys!");
+                        break;
+                    }
+                    index++;
+                }
+                if (index == boyNames.Length)
+                {
+                    //message box showing user input doesn't equal name in the file
+                    MessageBox.Show(boyTextBox.Text + " is NOT in the top 200 names for boys!");
+                }
+                
+            }
+            index = 0; //resets index in order for loop to run
+            if (string.IsNullOrEmpty(girlTextBox.Text) == girlInput) //tests if there is any name typed in girlTextBox
+            {
+
+                while (!girlSearchFile.EndOfStream) //initiate loop
+                {
+                    girlNames[index] = girlSearchFile.ReadLine(); //initialize 0 of the file to string
+                    if (girlName == girlNames[index]) //if number length of array equals user input then message box shows
+                    {
+                        //Message box showing user input equals a name in the file
+                        MessageBox.Show(girlTextBox.Text + " is in the top 200 names for girls!");
+                        break;
+                    }
+                    index++;
+                }
+                if (index == girlNames.Length) //runs if it gets through all 200 names and the name doesn't equal
+                {
+                    //message box showing user input doesn't equal name in the file
+                    MessageBox.Show(girlTextBox.Text + " is NOT in the top 200 names for girls!");
+                }
+
+            }
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            //close form
+            this.Close();
+        }
+    }
+}
